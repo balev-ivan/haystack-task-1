39ac6b42a31fa77d26fba3d708329615bf46bc9a
Ivan Balev <balev.ivan@digital-maintenance.com>
2022-05-19 21:58:37 +0300

    Version 3

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ window.onload = function () {
     initActiveTab();
 };
 
+const userTabLs = ["info_tab", "documents_tab", "id_verify_tab", "check_list_tab", "messages_tab", "notes_tab"];
+
 function initActiveTab() {
-    const defaultTabId = "transactions";
+    const defaultTabId = "transactions_tab";
+    const defaultUserTabId = "info_tab";
 
     // Global variable to store default selection
     let currentSelection = document.getElementById(defaultTabId);
+    let currentUserTab = document.getElementById(defaultUserTabId);
 
     const navLinks = document.querySelectorAll("div a");
 
@@ -20,13 +24,33 @@ function initActiveTab() {
 
     // Change active tab
     function changeActiveTab(targetAnchor) {
-        currentSelection.classList.remove("show-div");
         const selector = targetAnchor.replace("#", "");
-        const newTab = document.getElementById(selector);
-
-        currentSelection = newTab;
-        currentSelection.classList.add("show-div");
+        if (
+            userTabLs.includes(selector)
+        ) {
+            changeUserTab(targetAnchor)
+        } else {
+            currentSelection.classList.remove("show-div");
+            const newTab = document.getElementById(selector);
+            currentSelection = newTab;
+            currentSelection.classList.add("show-div");
+        }
+        ;
     }
+
+
+    // Change active tab
+    function changeUserTab(targetAnchor) {
+        const userSelector = targetAnchor.replace("#", "");
+        currentUserTab.classList.remove("show-div");
+        const newUserTab = document.getElementById(userSelector);
+        currentUserTab = newUserTab;
+        currentUserTab.classList.add("show-div");
+        if (userSelector === "documents_tab") {
+            genDivs();
+        }
+    }
+
 
     navLinks.forEach(function (e) {
         e.onclick = function () {
@@ -43,7 +67,17 @@ function initActiveTab() {
         } else changeActiveTab(defaultTabId);
     }
 
-    // Show default
+// Show default
     currentSelection.classList.add("show-div");
 }
 
+function genDivs() {
+    let UserDocs = document.getElementById("documents_grid");
+    for (let i = 1; i <= 6; i++) {
+
+        for (let j = 1; j <= 6; j++) {
+            UserDocs.innerHTML += '<div class="column">' + " i= " + i + " j= " + j + '</div>';
+        }
+
+    }
+}
